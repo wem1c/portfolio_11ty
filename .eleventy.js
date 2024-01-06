e40e8c8957f219116eb0610df5b06cae79f62c1d
Conor C. Peterson <conorpetersondev@gmail.com>
2024-01-06 21:16:56 +0100

    Hello World!

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+const { EleventyI18nPlugin } = require("@11ty/eleventy");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(EleventyI18nPlugin, {
+    defaultLanguage: "en",
+  });
+
+  eleventyConfig.addCollection("posts_en", function (collection) {
+    return collection.getFilteredByGlob("./_site/en/posts/*.md");
+  });
+
+  eleventyConfig.addCollection("posts_cnr", function (collection) {
+    return collection.getFilteredByGlob("./_site/cnr/posts/*.md");
+  });
+};
