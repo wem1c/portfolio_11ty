f13640949beed0f97bf1bfc2a496a9e82eea8a42
Conor C. Peterson <conorpetersondev@gmail.com>
2024-01-11 12:08:01 +0100

    feat(footer): update data

--- src/_data/footer.js
+++ src/_data/footer.js
@@ -1,20 +1,8 @@
-// TODO: update with actual data
 module.exports = {
-  mapUrl: "https://goo.gl/maps/3YTkhCgfEgj1PRAd7",
   cnr: {
-    addressTitle: "Adresa",
-    addressStreet: "Ulica Džordža Vašingtona",
-    addressNumber: "42",
-    addressPostcode: "85000",
-    addressCity: "Podgorica",
-    directionsLabel: "Upute",
+    signoff: "Napravio: dotični.",
   },
   en: {
-    addressTitle: "Address",
-    addressStreet: "Ulica Džordža Vašingtona",
-    addressNumber: "42",
-    addressPostcode: "85000",
-    addressCity: "Podgorica",
-    directionsLabel: "Directions",
+    signoff: "Made by yours truly.",
   },
 };
