7dcdd520c39d2ef223c590df96b131c8283efed3
Conor C. Peterson <conorpetersondev@gmail.com>
2024-01-11 14:04:28 +0100

    feat: add css bundler
    - create css-bundle.njk that bundles css to /bundle.css
    - install cssmin package to minify the css
    - create a includeAll shortcode that uses fast-glob package to include all css/* files

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,20 @@
+const CleanCSS = require("clean-css");
 const { EleventyI18nPlugin } = require("@11ty/eleventy");
+const fastGlob = require("fast-glob");
+const fs = require("fs");
+const nunjucks = require("nunjucks"); // Add this line
 
 module.exports = function (eleventyConfig) {
+  /**
+   * PLUGINS
+   */
   eleventyConfig.addPlugin(EleventyI18nPlugin, {
     defaultLanguage: "en",
   });
 
+  /**
+   * COLLECTIONS
+   */
   eleventyConfig.addCollection("posts_en", function (collection) {
     return collection.getFilteredByGlob("./src/en/posts/*.md");
   });
@@ -13,7 +23,37 @@ module.exports = function (eleventyConfig) {
     return collection.getFilteredByGlob("./src/cnr/posts/*.md");
   });
 
+  /**
+   * FILTERS
+   */
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+
+  /**
+   * SHORTCODES
+   */
+  eleventyConfig.addNunjucksShortcode("includeAll", function (globPattern) {
+    const files = fastGlob.sync(globPattern);
+    let result = "";
+
+    for (const file of files) {
+      result += `{% include "${file}" %}\n`;
+    }
+
+    return result;
+  });
+
+  /**
+   * PASSTHROUGH COPIES
+   */
+  //eleventyConfig.addPassthroughCopy("./src/fonts");
+  //eleventyConfig.addPassthroughCopy("./src/images");
+
   return {
     dir: { input: "src" },
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
   };
 };
