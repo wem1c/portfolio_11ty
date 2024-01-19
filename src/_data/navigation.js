559853e85c1a7d334c887211042a4d2926a29dd7
Conor C. Peterson <conorpetersondev@gmail.com>
2024-01-19 13:08:20 +0100

    refactor(data.navigation): add explicit locale links

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,6 +1,22 @@
 module.exports = {
-  items: [
-    { name: "About", url: "/about" },
-    { name: "Blog", url: "/blog" },
+  en: [
+    {
+      text: "About me",
+      url: "/en/about-me/",
+    },
+    {
+      text: "Blog",
+      url: "/en/blog/",
+    },
+  ],
+  cnr: [
+    {
+      text: "O meni",
+      url: "/cnr/o-meni/",
+    },
+    {
+      text: "Blog",
+      url: "/cnr/blog/",
+    },
   ],
 };
