a0a66d0c3239ab1d1d7bdb20d339f6fc810ca411
Conor C. Peterson <conorpetersondev@gmail.com>
2024-01-20 12:34:10 +0100

    feat: add project data
    - Remove placeholder image
    - Add projects.js data file to _data/
    - Add project images to assets/images/projects

---
+++ src/_data/projects.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    title: "Kraljevi Kvota",
+    url: "https://kraljevi-kvota.vercel.app/en",
+    imageSrc: "./src/assets/images/projects/kraljevi_kvota.png",
+  },
+  {
+    title: "Data Technologies",
+    url: "https://data-technologies-lac.vercel.app/",
+    imageSrc: "./src/assets/images/projects/data_technologies.png",
+  },
+  {
+    title: "Responsive Pokemon Chart",
+    url: "https://pokemon-type-chart.vercel.app/",
+    imageSrc: "./src/assets/images/projects/responsive_pokemon_chart.png",
+  },
+];
