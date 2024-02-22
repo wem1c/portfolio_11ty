0a983c022b33dfc9203758b670b4e88a8d9a6bce
Conor C. Peterson <conorpetersondev@gmail.com>
2024-02-22 17:30:27 +0100

    feat(data/dictionary): add bio

--- src/_data/dictionary.js
+++ src/_data/dictionary.js
@@ -1,3 +1,19 @@
 module.exports = {
+  bio: {
+    en: [
+      "Hello there! My name is Conor and this is my humble abode on the internet. 🏡",
+      "I'm passionate about creating websites and web applications from start to finish, which is why I specialize in Full-Stack Development. From crafting intuitive user interfaces to designing robust APIs and databases, I enjoy taking on every aspect of a project. But my interests and skills extend beyond just web development. I approach all engineering challenges with the same enthusiasm and dedication to quality.",
+      "I'm a true technology enthusiast, always eager to explore new frontiers and expand my knowledge in the world of computing. While my current carrer is in full-stack web development, I'm constantly learning and experimenting with new tools and platforms. I'm proficient with Linux, and I'm currently honing my skills in Mobile and Cloud Development. I'm also passionate about exploring cutting-edge fields like AI and Machine Learning.",
+      "In my free time, I'm an avid reader of research papers, books, and articles on a wide range of tech topics, and I enjoy sharing my insights and opinions through my writing. Whether I'm coding up a web app or diving into the latest research on quantum computing, I'm always seeking new challenges and opportunities to grow as a developer.",
+      "I'm always excited to collaborate with clients on projects that challenge and inspire me. Whether you need help with building a new website or improving an existing one, feel free to email me and let's get a conversation going!",
+    ],
+    cnr: [
+      "Zdravo! Ja sam Conor i ovo je moje skromno boravište na internetu. 🏡",
+      "Strastveno se bavim kreiranjem veb sajtova i veb aplikacija od početka do kraja, zbog čega sam specijalizovan za Full-Stack Development. Od oblikovanja intuitivnih korisničkih interfejsa do dizajniranja robustnih API-ja i baza podataka, uživam u preuzimanju svakog aspekta projekta. Ali moji interesi i veštine proširuju se izvan web razvoja. Pristupam svim inženjerskim izazovima s istim entuzijazmom i posvećenošću prema kvalitetu.",
+      "Ja sam pravi entuzijasta za tehnologiju, uvijek željan istraživanja novih granica i proširivanja svog znanja u svijetu računarstva. Dok je moja trenutna karijera u full-stack web razvoju, stalno učim i eksperimentišem s novim alatima i platformama. Profikasno koristim Linux, a trenutno usavršavam svoje veštine u mobilnom i cloud razvoju. Takođe sam strastven u istraživanju cutting-edge oblasti poput AI i Machine Learning-a.",
+      "U slobodno vrijeme, strastveno čitam istraživačke radove, knjige i članke o širokom spektru tehnoloških tema, i uživam u deljenju svojih uvida i mišljenja kroz svoje pisanje. Bilo da kodiram web aplikaciju ili se udubljujem u najnovija istraživanja o kvantnom računarstvu, uvijek tražim nove izazove i prilike za rast kao developer.",
+      "Uvijek sam uzbuđen zbog saradnje s klijentima na projektima koji me izazivaju i inspirišu. Ako vam treba pomoć u izgradnji novog veb sajta ili poboljšanju postojećeg, slobodno mi pošaljite e-mail i započnimo razgovor!",
+    ],
+  },
   "coming-soon": { en: "Coming Soon", cnr: "Uskoro" },
 };
