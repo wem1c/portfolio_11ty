7692270e909ec41636135aec935b372ca5d0dbe7
Conor C. Peterson <conorpetersondev@gmail.com>
2024-04-06 00:45:07 +0200

    feat(data/projects): update project data
    Includes:
    - adding two project images

--- src/_data/projects.js
+++ src/_data/projects.js
@@ -1,13 +1,26 @@
 module.exports = [
   {
-    title: "Kraljevi Kvota",
-    url: "https://kraljevi-kvota.vercel.app/en",
-    imageSrc: "./src/assets/images/projects/kraljevi_kvota.png",
+    title: "Soon/Ubrzo...",
+    url: "#projects",
+    imageSrc: "./src/assets/images/projects/in_progress.png",
+    disabled: true,
   },
   {
-    title: "Data Technologies",
-    url: "https://data-technologies-lac.vercel.app/",
-    imageSrc: "./src/assets/images/projects/data_technologies.png",
+    title: "Soon/Ubrzo...",
+    url: "#projects",
+    imageSrc: "./src/assets/images/projects/in_progress.png",
+    disabled: true,
+  },
+  {
+    title: "Soon/Ubrzo...",
+    url: "#projects",
+    imageSrc: "./src/assets/images/projects/in_progress.png",
+    disabled: true,
+  },
+  {
+    title: "ETF Obavijesti",
+    url: "https://etf-obavijesti.vercel.app/",
+    imageSrc: "./src/assets/images/projects/etf_obavijesti.png",
   },
   {
     title: "Responsive Pokemon Chart",
