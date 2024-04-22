84bb8d00e129b8e071975b5f837cc3801c36e5ac
Conor C. Peterson <conorpetersondev@gmail.com>
2024-04-22 11:09:43 +0200

    refactor(projects): move image alt data to data/projects.js file

--- src/_data/projects.js
+++ src/_data/projects.js
@@ -3,34 +3,43 @@ module.exports = [
     title: "Navy Blue",
     url: "https://navyblue.me/",
     imageSrc: "./src/assets/images/projects/navy_blue.png",
+    imageAlt: "Screenshot of Navy Blue's website homepage",
   },
   {
     title: "ETF Obavijesti",
     url: "https://etf-obavijesti.vercel.app/",
     imageSrc: "./src/assets/images/projects/etf_obavijesti.png",
+    imageAlt: "Screenshot of the ETF Obavijesti website homepage",
   },
   {
     title: "Responsive Pokemon Chart",
     url: "https://pokemon-type-chart.vercel.app/",
     imageSrc: "./src/assets/images/projects/responsive_pokemon_chart.png",
+    imageAlt: "Screenshot of chart depicting Pokemon type weaknesses",
   },
   // In progress placeholders
   {
     title: "JD",
     url: "#projects",
     imageSrc: "./src/assets/images/projects/in_progress.png",
+    imageAlt:
+      "A green background with white text on top that says 'Project in progress...'",
     disabled: true,
   },
   {
     title: "KK",
     url: "#projects",
     imageSrc: "./src/assets/images/projects/in_progress.png",
+    imageAlt:
+      "A green background with white text on top that says 'Project in progress...'",
     disabled: true,
   },
   {
     title: "DT",
     url: "#projects",
     imageSrc: "./src/assets/images/projects/in_progress.png",
+    imageAlt:
+      "A green background with white text on top that says 'Project in progress...'",
     disabled: true,
   },
 ];
